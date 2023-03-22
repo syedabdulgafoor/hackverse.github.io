b81f8077de91a54f2883ac0962eddf1fa5122db4
Sharvani2002 <sharvanilaxmisomayaji@gmail.com>
2023-03-23 02:11:13 +0530

    Added highlights from previous edition

--- main.js
+++ main.js
@@ -270,5 +270,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 
+// Testimonials 3D
+var angle = 0;
+function galleryspin(sign) { 
+spinner = document.querySelector("#spinner");
+if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+}
+
+
 
 
